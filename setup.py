97d07937ace032c02d62909dcddc47399420df4c
Ryan Stuart <ryan@mammothlabs.com.au>
2013-05-14 13:46:07 +1000

    Added the setup.py file and configured tox.
    Change-Id: Ib7d5e30e45fce705c9ef68fc5d632967d8548409

---
+++ setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+
+class Tox(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        errno = tox.cmdline(self.test_args)
+        sys.exit(errno)
+
+
+requires = [
+    'nltk',        
+    'numpy',        
+    'regex',        
+]
+
+setup(
+    name='caterpillar',
+    version='0.1',
+    packages=['caterpillar'],
+    url='http://www.mammothlabs.com.au',
+    license='Commercial',
+    install_requires=requires,
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
+    author='Mammoth Labs',
+    author_email='contact@mammothlabs.com.au',
+    description='Mammoth Labs text analytics engine.'
+)
