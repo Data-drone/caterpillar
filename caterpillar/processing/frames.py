13ba42eab2b9d74190887d29f5f8845fb12b900d
Ryan Stuart <ryan@mammothlabs.com.au>
2013-07-05 09:26:11 +1000

    Extracting frames from CSV
    Adding code to support the parsing of CSV files. The CSV files need to
    be broken down into their composite frames. Each frame needs all the file
    leve metdata and row level meta data stored against it. A row may have
    more then 1 text column.
    
    The approach taken here is one of pre-defined structure. A ColumnSpec
    must be passed into the frame_stream_csv method so that we know how to
    handle the csv file.
    
    Issue: http://agilezen.com/project/54516/story/12
    Change-Id: If670aab7c541b4c3a744da9a41e203e4240121f5

--- caterpillar/processing/frames.py
+++ caterpillar/processing/frames.py
@@ -1,7 +1,9 @@
-# caterpillar: Tools to work with text frames
+# caterpillar: Tools to work with and extract text frames
 #
 # Copyright (C) 2012-2013 Mammoth Labs
-# Author: Ryan Stuart <ryan@mammothlabs.com.au>
+# Author: Ryan Stuart <ryan@mammothlabs.com.au>, Kris Rogers <kris@mammothlabs.com.au>
+from StringIO import StringIO
+import csv
 
 import uuid
 from caterpillar.processing.tokenize import ParagraphTokenizer
@@ -9,18 +11,60 @@ from caterpillar.processing.tokenize import ParagraphTokenizer
 import nltk.data
 
 
+class ColumnDataType(object):
+    """
+    A ColumnDataType object identifies different data types in a CSV column.
+
+    There are five possible data types:
+    FLOAT -- A floating point number. Should be a string in a format that float() can parse.
+    INTEGER -- A integer. Should be in a format that int() can parse.
+    STRING -- A string type. This type ISN'T analysed. It is just stored.
+    TEXT -- A string type. This is like STRING but it IS analysed and stored (it is used to generate a frame stream).
+    IGNORE -- Ignore this column.
+
+    """
+    FLOAT = 1
+    INTEGER = 2
+    STRING = 3
+    TEXT = 4
+    IGNORE = 5
+
+
+class ColumnSpec(object):
+    """
+    A ColumnSpec object represents information about a column in a CSV file.
+
+    This includes the column's name and its type.
+
+    """
+
+    def __init__(self, name, type):
+        """
+        Create a new ColumnSpec. A ColumnSpec must have a name and a type.
+
+        Required
+        name -- A string name for this column.
+        type -- The type of this column as a ColumnDataType object.
+
+        """
+        self.name = name
+        self.type = type
+
+
 class Frame(object):
     """
-    A frame is a piece of text who's size is measured in sentences. A frame has a minimum size of 1. A frame also has
-    additional information about itself including a dict of word frequencies, a dict of metadata, a string
-    representation of its original form, a unique identifier, a sequence number and optionally a list of unique words.
+    A frame is a piece of text who's size is measured in sentences.
+
+    A frame has a minimum size of 1. A frame also has additional information about itself including a dict of word
+    frequencies, a dict of metadata, a string representation of its original form, a unique identifier, a sequence
+    number and optionally a list of unique words.
 
     """
 
     def __init__(self, id, sequence, text, metadata=None, unique_words=None):
         """
         Create a new frame from the passed id string, sequence int, text string, frequencies dict and optional metadata
-        dict.
+        dict, unique_words dict.
 
         """
         self.id = id
@@ -35,42 +79,117 @@ WINDOW_SIZE = 1024*1024*10  # our sliding window of text will be 10MB big
 
 def frame_stream(text_file, frame_size=2, tokenizer=nltk.data.load('tokenizers/punkt/english.pickle'), meta_data=None,
                  encoding='utf-8'):
-
     """
     This generator function yields text frames parsed from text_file.
 
     A frame is defined as a block of text who's size is measured in sentences and is at least on sentence long. A frame
-    can have meta data associated with it. Some of this meta data is passed to this function directly (original document
-    name as text for example or maybe even document author). Other meta data will be emergent from the text frames
-    themselves.
+    can have metadata associated with it. Some of this metadata is passed to this function directly (original document
+    name as text for example or maybe even document author). Other metadata could possibly be emergent from the text
+    frames themselves.
 
     Required arguments:
-    text_file -- a file object returned by something like open() where the text data is to be read from.
+    text_file -- A file object returned by something like open() where the text data is to be read from.
 
     Keyword arguments:
-    frame_size -- the size of the frames to yield as an int (default 2)
-    tokenizer -- an object instance with a tokenize method that accepts a string and returns sentences.
+    frame_size -- The size of the frames to yield as an int (default 2). If this argument is less than 1 then only
+                  1 frame is returned containing all the text in the passed file like object.
+    tokenizer -- An object instance with a tokenize method that accepts a string and returns sentences.
+    meta_data -- A dict of meta data values.
+    encoding -- The encoding of the strings read from text_file.
 
     """
-    window = u""
-    input = text_file.read(WINDOW_SIZE)
     sequence_number = 1
-    while input:
-        window += input.decode(encoding)
+    if frame_size > 0:
+        # Break text up into frames of frame_size sentences long
+        window = u""
+        input = text_file.read(WINDOW_SIZE)
+        while input:
+            window += input.decode(encoding)
+            paragraphs = ParagraphTokenizer().tokenize(window)
+            for paragraph in paragraphs[:-1]:  # Never tokenize the last paragraph in case it isn't complete
+                sentences = tokenizer.tokenize(paragraph, realign_boundaries=True)
+                frames_text = (" ".join(sentences[i:i+frame_size]) for i in xrange(0, len(sentences), frame_size))
+                for text in frames_text:
+                    yield Frame(uuid.uuid4(), sequence_number, text, meta_data)
+                    sequence_number += 1
+            window = window[window.rfind(paragraphs[-1]):]
+            window = window.lstrip()  # Don't want it starting with spaces!
+            input = text_file.read(WINDOW_SIZE - len(window))
         paragraphs = ParagraphTokenizer().tokenize(window)
-        for paragraph in paragraphs[:-1]:  # Never tokenize the last paragraph in case it isn't complete
+        for paragraph in paragraphs:
             sentences = tokenizer.tokenize(paragraph, realign_boundaries=True)
             frames_text = (" ".join(sentences[i:i+frame_size]) for i in xrange(0, len(sentences), frame_size))
             for text in frames_text:
                 yield Frame(uuid.uuid4(), sequence_number, text, meta_data)
                 sequence_number += 1
-        window = window[window.rfind(paragraphs[-1]):]
-        window = window.lstrip()  # Don't want it starting with spaces!
-        input = text_file.read(WINDOW_SIZE - len(window))
-    paragraphs = ParagraphTokenizer().tokenize(window)
-    for paragraph in paragraphs:
-        sentences = tokenizer.tokenize(paragraph, realign_boundaries=True)
-        frames_text = (" ".join(sentences[i:i+frame_size]) for i in xrange(0, len(sentences), frame_size))
-        for text in frames_text:
-            yield Frame(uuid.uuid4(), sequence_number, text, meta_data)
-            sequence_number += 1
+    else:
+        # Return all text in 1 frame
+        input = text_file.read().decode(encoding)
+        yield Frame(uuid.uuid4(), sequence_number, input.strip(), meta_data)
+
+
+def frame_stream_csv(csv_file, column_spec, frame_size=2, tokenizer=nltk.data.load('tokenizers/punkt/english.pickle'),
+                     meta_data=dict(), encoding='utf-8', delimiter=',', quotechar='"'):
+    """
+    This generator function yields text frames parsed from csv_file.
+
+    A frame is defined as a block of text who's size is measured in sentences and is at least on sentence long. A frame
+    can have metadata associated with it. Some of this metadata is passed to this function directly (original document
+    name as text for example or maybe even document author). Other metadata could be emergent from the text frames
+    themselves. In the case of CSV files, all values in non-textual cells will be captured as metadata.
+
+    To parse a CSV file this function needs a column_spec which is a list of ColumnSpec objects. The order of the
+    ColumnSpec objects in the passed list must correspond to the order of columns in the passed csv_file. All columns
+    must be accounted for in the passed ColumnSpec list. If you aren't interested in a column, set it to
+    ColumnDataType.IGNORE.
+
+    Required arguments:
+    csv_file -- A file like object returned by something like open() where the text data is to be read from. If this
+                is a file object it must be opened with the 'rbU' flag.
+    column_spec -- A list of ColumnSpec objects in column order.
+
+    Keyword arguments:
+    frame_size -- The size of the frames to yield from text cells as an int (default 2). If this argument is less than 1
+                  then all the text in a cell will be returned as a single frame.
+    tokenizer -- An object instance with a tokenize method that accepts a string and returns sentences.
+    meta_data -- A dict of meta data values for this file.
+    encoding -- The encoding of the strings read from text_file.
+    delimiter -- A one-character string used to separate fields. It defaults to ','.
+
+    """
+    # Try and guess a dialect by parsing a sample
+    snipit = csv_file.read(4096)
+    sniffer = csv.Sniffer()
+    dialect = sniffer.sniff(snipit, delimiter)
+
+    # Reset back to the start of the file and actually read it
+    csv_file.seek(0)
+    csv_reader = csv.reader(csv_file, dialect)
+    # Don't parse the header
+    if sniffer.has_header(snipit):
+        csv_reader.next()
+    # Do the actual work. Go through row-by-row then cell-by-cell looking at the data type for each cell. If it is a
+    # TEXT cell, then add it to a queue for this row. Otherwise, if it isn't an IGNORE cell, add it to the meta data
+    # for this row. Then, return to the queue of TEXT columns and extract teh frames from each passing in the discovered
+    # meta data.
+    row_seq = 1  # Might be interesting to have, so store it
+    for row in csv_reader:
+        row_meta_data = meta_data.copy()
+        row_meta_data['row_seq'] = str(row_seq)
+        row_seq += 1
+        text_queue = []
+        index = 0
+        for cell in row:
+            if column_spec[index].type != ColumnDataType.IGNORE and cell:
+                if column_spec[index].type == ColumnDataType.TEXT:
+                    text_queue.append((column_spec[index].name, cell))
+                else:
+                    row_meta_data[column_spec[index].name] = cell
+            index += 1
+        # Process the TEXT cells
+        for text_cell in text_queue:
+            cell_meta_data = row_meta_data.copy()
+            cell_meta_data['column'] = text_cell[0]
+            for frame in frame_stream(StringIO(text_cell[1]), frame_size=frame_size, tokenizer=tokenizer,
+                                      meta_data=cell_meta_data, encoding=encoding):
+                yield frame
--- caterpillar/processing/test/test_frames.py
+++ caterpillar/processing/test/test_frames.py
@@ -1,14 +1,15 @@
-# caterpillar: Tests for the caterpillar.processing.frame module
+# caterpillar: Tests for the caterpillar.processing.frames module
 #
 # Copyright (C) 2012-2013 Mammoth Labs
 # Author: Ryan Stuart <ryan@mammothlabs.com.au>
 
 
 import os
-from caterpillar.processing.frames import frame_stream
+from caterpillar.processing.frames import *
 
 
 def test_frame_stream_alice():
+    """Test frame extraction on Chapter 1 of Alice in Wonderland."""
     frames = list(frame_stream(open(os.path.abspath('caterpillar/processing/test/alice_test_data.txt'), 'r'),
                                meta_data={'document': 'alice_test_data.txt'}))
     assert len(frames) == 49
@@ -16,7 +17,59 @@ def test_frame_stream_alice():
 
 
 def test_frame_stream_economics():
+    """Test frame extraction on a Wikipedia page about Economics."""
     frames = list(frame_stream(open(os.path.abspath('caterpillar/processing/test/economics_test_data.txt'), 'r'),
                                meta_data={'document': 'economics_test_data.txt'}))
     assert len(frames) == 7
     assert frames[0].metadata['document'] == 'economics_test_data.txt'
+
+
+def test_frame_stream_csv_regular():
+    """Test CSV frame extraction using some customer feedback data."""
+    columns = [ColumnSpec('respondant', ColumnDataType.INTEGER),
+               ColumnSpec('region', ColumnDataType.STRING),
+               ColumnSpec('store', ColumnDataType.STRING),
+               ColumnSpec('liked', ColumnDataType.TEXT),
+               ColumnSpec('disliked', ColumnDataType.TEXT),
+               ColumnSpec('would_like', ColumnDataType.TEXT),
+               ColumnSpec('nps', ColumnDataType.INTEGER)]
+    frames = list(frame_stream_csv(open(os.path.abspath('caterpillar/processing/test/test_small.csv'), 'rbU'), columns,
+                  meta_data={'document': 'test_small.csv'}))
+    assert len(frames) == 39
+    assert frames[0].metadata['document'] == 'test_small.csv'
+    assert frames[0].metadata['row_seq'] == '1'
+    assert frames[14].metadata['nps'] == '1'
+
+
+def test_frame_stream_csv_with_cr():
+    """Test CSV frame extraction using customer feedback data with OSX style newline characters."""
+    columns = [ColumnSpec('respondant', ColumnDataType.INTEGER),
+               ColumnSpec('region', ColumnDataType.STRING),
+               ColumnSpec('store', ColumnDataType.STRING),
+               ColumnSpec('liked', ColumnDataType.TEXT),
+               ColumnSpec('disliked', ColumnDataType.TEXT),
+               ColumnSpec('would_like', ColumnDataType.TEXT),
+               ColumnSpec('nps', ColumnDataType.INTEGER)]
+    frames = list(frame_stream_csv(open(os.path.abspath('caterpillar/processing/test/test_with_CR.csv'), 'rbU'), columns,
+                  meta_data={'document': 'test_small.csv'}))
+    assert len(frames) == 39
+    assert frames[0].metadata['document'] == 'test_small.csv'
+    assert frames[0].metadata['row_seq'] == '1'
+    assert frames[14].metadata['nps'] == '1'
+
+
+def test_frame_stream_csv_cell_as_frame():
+    """Text CSV frame extraction with frame size set to 0 (treat each cell as a frame no matter the length)."""
+    columns = [ColumnSpec('respondant', ColumnDataType.INTEGER),
+               ColumnSpec('region', ColumnDataType.STRING),
+               ColumnSpec('store', ColumnDataType.STRING),
+               ColumnSpec('liked', ColumnDataType.TEXT),
+               ColumnSpec('disliked', ColumnDataType.TEXT),
+               ColumnSpec('would_like', ColumnDataType.TEXT),
+               ColumnSpec('nps', ColumnDataType.INTEGER)]
+    frames = list(frame_stream_csv(open(os.path.abspath('caterpillar/processing/test/test_small.csv'), 'rbU'), columns,
+                  meta_data={'document': 'test_small.csv'}, frame_size=0))
+    assert len(frames) == 35
+    assert frames[0].metadata['document'] == 'test_small.csv'
+    assert frames[0].metadata['row_seq'] == '1'
+    assert frames[14].metadata['nps'] == '1'
