8ce4c05462b65d14a26f73343280770abef36856
Kris Rogers <kris@kapiche.com>
2014-05-27 17:16:39 +1000

    Added term statistics values to topics in topical classification.
    Available statitics are topic-based relative frequency and relative
    influence for terms.

--- examples/topical_classification.py
+++ examples/topical_classification.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2012-2013 Mammoth Labs
 # Author: Ryan Stuart <ryan@mammothlabs.com.au>
 import os
-from caterpillar.analytics.influence import InfluenceAnalyticsPlugin
+from caterpillar.analytics.influence import InfluenceAnalyticsPlugin, InfluenceTopicsPlugin
 from caterpillar.processing.analysis.analyse import DefaultAnalyser
 from caterpillar.processing.index import Index
 from caterpillar.processing.schema import Schema, TEXT
@@ -14,4 +14,12 @@ with open(os.path.abspath('caterpillar/test_resources/alice.txt'), 'r') as f:
     data = f.read()
     index.add_document(fold_case=True, text=data)
     index.run_plugin(InfluenceAnalyticsPlugin, influence_factor_smoothing=True)
-    topical_classification = InfluenceAnalyticsPlugin(index).get_topical_classification()
+    topics_plugin = index.run_plugin(InfluenceTopicsPlugin)
+    topical_classification = topics_plugin.get_topical_classification()
+
+    for topic in topical_classification.topics:
+        print topic.name
+        for term in topic.terms:
+            print '\t{}'.format(term.value), round(topic.term_statistics[term.value].relative_frequency, 3),\
+                round(topic.term_statistics[term.value].relative_influence, 3)
+        print '\n'
