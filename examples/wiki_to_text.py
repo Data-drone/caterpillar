abd0aab8b077521b66ac140b65a3ae884f986fce
Ryan Stuart <ryan@kapiche.com>
2014-09-05 11:16:13 +1000

    Moving to a different parsing method for wiki text.

--- examples/wiki_to_text.py
+++ examples/wiki_to_text.py
@@ -1,16 +1,253 @@
 # Copyright (c) 2012-2014 Kapiche Limited
 # Author: Ryan Stuart <ryan@kapiche.com>
+import bz2
 import csv
+import re
+from xml.etree.cElementTree import iterparse
+
 import begin
-from mw.xml_dump import Iterator
-from mw.xml_dump.element_iterator import ElementIterator
+
+try:
+    from html.entities import name2codepoint as n2cp
+except ImportError:
+    from htmlentitydefs import name2codepoint as n2cp
+
+# ignore articles shorter than ARTICLE_MIN_WORDS characters (after full preprocessing)
+ARTICLE_MIN_WORDS = 50
+
+# HTML
+RE_HTML_ENTITY = re.compile(r'&(#?)(x?)(\w+);', re.UNICODE)
+
+# Wiki markup
+RE_P0 = re.compile('<!--.*?-->', re.DOTALL | re.UNICODE) # comments
+RE_P1 = re.compile('<ref([> ].*?)(</ref>|/>)', re.DOTALL | re.UNICODE) # footnotes
+RE_P2 = re.compile("(\n\[\[[a-z][a-z][\w-]*:[^:\]]+\]\])+$", re.UNICODE) # links to languages
+RE_P3 = re.compile("{{([^}{]*)}}", re.DOTALL | re.UNICODE) # template
+RE_P4 = re.compile("{{([^}]*)}}", re.DOTALL | re.UNICODE) # template
+RE_P5 = re.compile('\[(\w+):\/\/(.*?)(( (.*?))|())\]', re.UNICODE) # remove URL, keep description
+RE_P6 = re.compile("\[([^][]*)\|([^][]*)\]", re.DOTALL | re.UNICODE) # simplify links, keep description
+RE_P7 = re.compile('\n\[\[[iI]mage(.*?)(\|.*?)*\|(.*?)\]\]', re.UNICODE) # keep description of images
+RE_P8 = re.compile('\n\[\[[fF]ile(.*?)(\|.*?)*\|(.*?)\]\]', re.UNICODE) # keep description of files
+RE_P9 = re.compile('<nowiki([> ].*?)(</nowiki>|/>)', re.DOTALL | re.UNICODE) # outside links
+RE_P10 = re.compile('<math([> ].*?)(</math>|/>)', re.DOTALL | re.UNICODE) # math content
+RE_P11 = re.compile('<(.*?)>', re.DOTALL | re.UNICODE) # all other tags
+RE_P12 = re.compile('\n(({\|)|(\|-)|(\|}))(.*?)(?=\n)', re.UNICODE) # table formatting
+RE_P13 = re.compile('\n(\||\!)(.*?\|)*([^|]*?)', re.UNICODE) # table cell formatting
+RE_P14 = re.compile('\[\[Category:[^][]*\]\]', re.UNICODE) # categories
+# Remove File and Image template
+RE_P15 = re.compile('\[\[([fF]ile:|[iI]mage)[^]]*(\]\])', re.UNICODE)
+
+
+def to_unicode(text, encoding='utf8', errors='strict'):
+    """Convert a string (bytestring in `encoding` or unicode), to unicode."""
+    if isinstance(text, unicode):
+        return text
+    return unicode(text, encoding, errors=errors)
+
+
+def decode_htmlentities(text):
+    """
+    Decode HTML entities in text, coded as hex, decimal or named.
+
+    Adapted from http://github.com/sku/python-twitter-ircbot/blob/321d94e0e40d0acc92f5bf57d126b57369da70de/html_decode.py
+
+    >>> print(decode_htmlentities("l&#39;eau"))
+    l'eau
+    >>> print(decode_htmlentities("foo &lt; bar"))
+    foo < bar
+
+    """
+    def substitute_entity(match):
+        ent = match.group(3)
+        if match.group(1) == "#":
+            # decoding by number
+            if match.group(2) == '':
+                # number is in decimal
+                return unichr(int(ent))
+            elif match.group(2) == 'x':
+                # number is in hex
+                return unichr(int('0x' + ent, 16))
+        else:
+            # they were using a name
+            cp = n2cp.get(ent)
+            if cp:
+                return unichr(cp)
+            else:
+                return match.group()
+
+    try:
+        return RE_HTML_ENTITY.sub(substitute_entity, text)
+    except:
+        # in case of errors, return input
+        # e.g., ValueError: unichr() arg not in range(0x10000) (narrow Python build)
+        return text
+
+
+def filter_wiki(raw):
+    """
+    Filter out wiki mark-up from `raw`, leaving only text. `raw` is either unicode
+    or utf-8 encoded string.
+    """
+    # parsing of the wiki markup is not perfect, but sufficient for our purposes
+    # contributions to improving this code are welcome :)
+    text = to_unicode(raw, 'utf8', errors='ignore')
+    text = decode_htmlentities(text)  # '&amp;nbsp;' --> '\xa0'
+    return remove_markup(text)
+
+
+def remove_markup(text):
+    text = re.sub(RE_P2, "", text)  # remove the last list (=languages)
+    # the wiki markup is recursive (markup inside markup etc)
+    # instead of writing a recursive grammar, here we deal with that by removing
+    # markup in a loop, starting with inner-most expressions and working outwards,
+    # for as long as something changes.
+    text = remove_template(text)
+    text = remove_file(text)
+    iters = 0
+    while True:
+        old, iters = text, iters + 1
+        text = re.sub(RE_P0, "", text)  # remove comments
+        text = re.sub(RE_P1, '', text)  # remove footnotes
+        text = re.sub(RE_P9, "", text)  # remove outside links
+        text = re.sub(RE_P10, "", text)  # remove math content
+        text = re.sub(RE_P11, "", text)  # remove all remaining tags
+        text = re.sub(RE_P14, '', text)  # remove categories
+        text = re.sub(RE_P5, '\\3', text)  # remove urls, keep description
+        text = re.sub(RE_P6, '\\2', text)  # simplify links, keep description only
+        # remove table markup
+        text = text.replace('||', '\n|')  # each table cell on a separate line
+        text = re.sub(RE_P12, '\n', text)  # remove formatting lines
+        text = re.sub(RE_P13, '\n\\3', text)  # leave only cell content
+        # remove empty mark-up
+        text = text.replace('[]', '')
+        if old == text or iters > 2:  # stop if nothing changed between two iterations or after a fixed number of iterations
+            break
+
+    # the following is needed to make the tokenizer see '[[socialist]]s' as a single word 'socialists'
+    # TODO is this really desirable?
+    text = text.replace('[', '').replace(']', '') # promote all remaining markup to plain text
+    return text
+
+
+def remove_template(s):
+    """Remove template wikimedia markup.
+
+    Return a copy of `s` with all the wikimedia markup template removed. See
+    http://meta.wikimedia.org/wiki/Help:Template for wikimedia templates
+    details.
+
+    Note: Since template can be nested, it is difficult remove them using
+    regular expresssions.
+    """
+
+    # Find the start and end position of each template by finding the opening
+    # '{{' and closing '}}'
+    n_open, n_close = 0, 0
+    starts, ends = [], []
+    in_template = False
+    prev_c = None
+    for i, c in enumerate(iter(s)):
+        if not in_template:
+            if c == '{' and c == prev_c:
+                starts.append(i - 1)
+                in_template = True
+                n_open = 1
+        if in_template:
+            if c == '{':
+                n_open += 1
+            elif c == '}':
+                n_close += 1
+            if n_open == n_close:
+                ends.append(i)
+                in_template = False
+                n_open, n_close = 0, 0
+        prev_c = c
+
+    # Remove all the templates
+    s = ''.join([s[end + 1:start] for start, end in
+                 zip(starts + [None], [-1] + ends)])
+
+    return s
+
+
+def remove_file(s):
+    """Remove the 'File:' and 'Image:' markup, keeping the file caption.
+
+    Return a copy of `s` with all the 'File:' and 'Image:' markup replaced by
+    their corresponding captions. See http://www.mediawiki.org/wiki/Help:Images
+    for the markup details.
+    """
+    # The regex RE_P15 match a File: or Image: markup
+    for match in re.finditer(RE_P15, s):
+        m = match.group(0)
+        caption = m[:-2].split('|')[-1]
+        s = s.replace(m, caption, 1)
+    return s
+
+
+def get_namespace(tag):
+    """Returns the namespace of tag."""
+    m = re.match("^{(.*?)}", tag)
+    namespace = m.group(1) if m else ""
+    if not namespace.startswith("http://www.mediawiki.org/xml/export-"):
+        raise ValueError("%s not recognized as MediaWiki dump namespace"
+                         % namespace)
+    return namespace
+_get_namespace = get_namespace
+
+
+def extract_pages(f, filter_namespaces=False):
+    """
+    Extract pages from MediaWiki database dump.
+
+    Returns
+    -------
+    pages : iterable over (str, str)
+        Generates (title, content) pairs.
+    """
+    elems = (elem for _, elem in iterparse(f, events=("end",)))
+
+    # We can't rely on the namespace for database dumps, since it's changed
+    # it every time a small modification to the format is made. So, determine
+    # those from the first element we find, which will be part of the metadata,
+    # and construct element paths.
+    elem = next(elems)
+    namespace = get_namespace(elem.tag)
+    ns_mapping = {"ns": namespace}
+    page_tag = "{%(ns)s}page" % ns_mapping
+    text_path = "./{%(ns)s}revision/{%(ns)s}text" % ns_mapping
+    title_path = "./{%(ns)s}title" % ns_mapping
+    ns_path = "./{%(ns)s}ns" % ns_mapping
+    pageid_path = "./{%(ns)s}id" % ns_mapping
+
+    for elem in elems:
+        if elem.tag == page_tag:
+            title = elem.find(title_path).text
+            text = elem.find(text_path).text
+
+            ns = elem.find(ns_path).text
+            if filter_namespaces and ns not in filter_namespaces:
+                text = None
+
+            pageid = elem.find(pageid_path).text
+            yield title, text or "", pageid     # empty page will yield None
+
+            # Prune the element tree, as per
+            # http://www.ibm.com/developerworks/xml/library/x-hiperfparse/
+            # except that we don't need to prune backlinks from the parent
+            # because we don't use LXML.
+            # We do this only for <page>s, since we need to inspect the
+            # ./revision/text element. The pages comprise the bulk of the
+            # file, so in practice we prune away enough.
+            elem.clear()
 
 
 @begin.start
 @begin.convert(num_of_articles=int, step_size=int)
-def run(xml_dump, output_file, num_of_articles=0, step_size=10000):
-    el = ElementIterator.from_file(open(xml_dump))
-    dump = Iterator.from_element(el)
+def run(wiki_dump, output_file, num_of_articles=0, step_size=10000):
+    filter_namespaces = ('0',)
+    ignore_namespaces = 'Wikipedia Category File Portal Template MediaWiki User Help Book Draft'.split()
+    texts = ((text, title, pageid) for title, text, pageid in extract_pages(bz2.BZ2File(wiki_dump), filter_namespaces))
     file_count = 1
     count = 0
     real_count = 0
@@ -20,18 +257,11 @@ def run(xml_dump, output_file, num_of_articles=0, step_size=10000):
             with open(path, 'w') as f:
                 for _ in xrange(step_size):
                     writer = csv.writer(f)
-                    page = next(dump)
+                    page = next(texts)
                     real_count += 1
-                    if page.redirect is None:
+                    if len(page[0]) > 400 or not any(page[1].startswith(ignore + ':') for ignore in ignore_namespaces):
                         count += 1
-                        revision = next(page)
-                        if revision.text:
-                            row = [page.id, page.title, 0, revision.id, revision.text]
-                        else:
-                            row = [page.id, page.title, 0, revision.id, ""]
-                    else:
-                        row = [page.id, page.title, 1, '', '']
-                    writer.writerow(row)
+                        writer.writerow(page[2], page[1], page[0])
                     if real_count % step_size == 0:
                         print "Written out {:,} articles so far, {:,} real ({}).".format(real_count, count, path)
             if num_of_articles and real_count >= num_of_articles:
