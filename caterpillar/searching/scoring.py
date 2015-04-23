42de6bb423dae070982ed6719b0a66ff8d6bd0a9
Kris Rogers <kris@kapiche.com>
2015-04-24 09:49:00 +1000

    Updated tf-idf scorer to use vector space model.
    Turns out the previous tf-idf scoring was completely broken. It was only considering partial vectors in the tf-idf model based on the terms in the search query. This throws off the normalisation and results in much higher similarities than expected when using cosine similarity.

--- caterpillar/searching/scoring.py
+++ caterpillar/searching/scoring.py
@@ -1,7 +1,9 @@
 # Copyright (c) 2012-201 Kapiche Limited
 # Author: Kris Rogers <kris@kapiche.com>, Ryan Stuart <ryan@kapiche.com>
+from __future__ import division
 import abc
-import math
+
+import numpy
 
 
 class Scorer(object):
@@ -48,31 +50,59 @@ class SimpleScorer(Scorer):
 
 class TfidfScorer(Scorer):
     """
-    A scorer that uses TF-IDF.
+    A scorer that uses TF-IDF in a Vector Space Model.
 
     """
     def __init__(self, index):
-        self.num_frames = index.get_frame_count()
+        self.frame_indexes = {frame_id: i for i, frame_id in enumerate(list(index.get_frame_ids()))}
         self.term_positions = {k: v for k, v in index.get_positions_index()}
+        self.tfidf_index, self.tfidf_index_norm =TfidfScorer.build_tfidf_index(self.term_positions,
+            self.frame_indexes)
         super(TfidfScorer, self).__init__(index)
+
+    @staticmethod
+    def build_tfidf_index(term_positions, frame_indexes):
+        """
+        Given `term_positions` and `frame_indexes`, which maps each frame id to a numerical index,
+        generate a full tf-idf index for use in searching.
+
+        Returns a 2-tuple containing the generated tf-idf index, and,
+        a vector containing the norms of all frame vectors in the tf-idf index.
+
+        """
+        num_frames = len(frame_indexes)
+        idfs = numpy.array([
+            numpy.log((num_frames / len(positions) if positions else 0) + 1)
+            for positions in term_positions.itervalues()
+        ])
+        tfs = numpy.zeros((len(term_positions), num_frames))
+        term_index = 0
+        for term, frame_positions in term_positions.iteritems():
+            for frame_id, positions in frame_positions.iteritems():
+                tfs[term_index][frame_indexes[frame_id]] = len(positions)
+            term_index = term_index + 1
+
+        tfidf_index = tfs.T * idfs
+
+        return (tfidf_index, numpy.linalg.norm(tfidf_index, axis=1))
 
     def score_and_rank(self, hits, term_weights):
         """
-        Score hits and return in ranked order according to TF-IDF.
+        Score hits and return in ranked order according to TF-IDF in the VSM.
 
         """
-        idfs = self._compute_idfs(term_weights)
+        if len(term_weights) == 0:
+            # Handle metadata-only search where no scoring is necessary
+            return hits
+
+        # Calculate cosine similarity
+        query_weights = []
+        for term in self.term_positions.iterkeys():
+            query_weights.append(term_weights.get(term, 0))
+        scored_hits = numpy.dot(self.tfidf_index, query_weights) / (self.tfidf_index_norm *
+                                                                   numpy.linalg.norm(query_weights))
+
+        # Update scores and return ranked hits
         for hit in hits:
-            score = 0
-            for term in hit.frame_terms.intersection(term_weights.keys()):
-                score += len(self.term_positions[term][hit.frame_id]) * idfs[term] * term_weights[term]
-            hit.score = score
-
+            hit.score = scored_hits[self.frame_indexes[hit.frame_id]]
         return sorted(hits, key=lambda h: (h.score, len(h.frame_terms), h.frame_id), reverse=True)
-
-    def _compute_idfs(self, terms):
-        """
-        Compute inverse document frequencies for the specified terms.
-
-        """
-        return {term: math.log(self.num_frames / len(self.term_positions[term])) for term in terms}
